2a2d07737f8992e923f0946573bd400c17a4e872
justokaycoding <59494195+justokaycoding@users.noreply.github.com>
2021-02-06 16:33:23 -0600

    m

--- Models/about_model.php
+++ Models/about_model.php
@@ -2,9 +2,9 @@
 
 // * The about page model
 require_once __DIR__.'/model.php';
-
-class AboutModel extends Model{
 m
+class AboutModel extends Model{
+
   function __construct(){
     parent::__construct();
   }
