d2fdb177e11298d7ae64297fe530e246e85a5670
justokaycoding <59494195+justokaycoding@users.noreply.github.com>
2021-02-06 23:26:25 -0600

    Update view.php

--- Views/view.php
+++ Views/view.php
@@ -11,7 +11,7 @@ class View{
     }
 
     public function index(){
-      $file_name = 'http://localhost:8002/Template/header.html';
+      //$file_name = 'http://localhost:8000/Template/header.html';
       $file_content = file_get_contents($file_name);
       return $this->controller->getPageID();
     }
