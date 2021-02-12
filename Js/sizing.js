51ecced2c08028f67a31a4e9409479716124f6b9
justokaycoding <59494195+justokaycoding@users.noreply.github.com>
2021-02-11 18:39:41 -0600

    Sizing Update

---
+++ Js/sizing.js
@@ -0,0 +1,31 @@
+(function($) {
+
+  let elements = ['p.productTitle'];
+
+  $(document).ready(function() {
+    if (window.matchMedia('(max-width: 767px)').matches) return;
+      elements.forEach((item, i) => {
+          sizing(item);
+      });
+  });
+
+  $(window).resize(function(){
+      if (window.matchMedia('(max-width: 767px)').matches) return;
+      elements.forEach((item, i) => {
+          sizing(item);
+      });
+  });
+
+  function sizing($element){
+    var height = 0;
+    $($element).css('min-height', 'initial');
+    $($element).each(function() {
+      if ($(this).outerHeight() > height) {
+        height = $(this).outerHeight();
+      }
+    });
+
+    $($element).css('min-height', height + 'px');
+  }
+
+})(jQuery)
